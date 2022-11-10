9030d6b5b284e3f9416032d89d4dfb2ea9551701
Guglielmo Bordin <52690645+gulbord@users.noreply.github.com>
2022-11-10 11:51:46 +0100

    Add ex03 first script

---
+++ ex03-01.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# create subdir 'students' only if it doesn't exist
+mkdir -p students
+
+# download csv list of students if it isn't already there
+file=students/LCP_22-23_students.csv
+if [ ! -f $file ]; then
+    wget -q -O $file \
+    https://www.dropbox.com/s/867rtx3az6e9gm8/LCP_22-23_students.csv
+fi
+# remove DOS line endings
+sed -i "s/\r$//" $file
+
+# get filenames
+filename=${file%%.*}
+podfile=${filename}_PoD.csv
+physfile=${filename}_Physics.csv
+
+# add corresponding students
+grep "PoD" $file > $podfile
+grep "Physics" $file > $physfile
+
+# add header to both files
+header=$(head -n 1 $file)
+sed -i "1i$header" $podfile $physfile
+
+# count number of surnames starting with a given letter, and find max count
+maxcount=1
+maxletter=A
+tail -n+2 $file > noheadfile
+for c in {A..Z}; do
+    count=$(grep -c "^$c" noheadfile)
+    echo "$c --> $count"
+    if [ "$count" -gt "$maxcount" ]; then
+        maxcount=$count
+        maxletter=$c
+    fi
+done
+echo "The most common first letter in surnames is $maxletter"
+
+# if group files are not present, create them empty
+if [[ -n $(echo students/group*) ]]; then
+    for i in 0{1..9} {10..18}; do
+        touch students/group$i.csv
+    done
+else
+    exit 0
+fi
+
+# read student list line by line
+count=1
+while IFS='' read -r line; do
+    # get group number by line mod 18
+    groupnum=$(($count % 18))
+    if [ "$groupnum" -eq 0 ]; then
+        groupnum=18
+    fi
+    echo $line >> students/group$(printf %02d $groupnum).csv
+    count=$(($count + 1))
+done < noheadfile
+
+rm noheadfile
